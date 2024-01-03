eac32f1ad19d057bb4237238436d5df61f25bdb8
kirisaki <735647137@qq.com>
2024-01-03 15:58:04 +0800

    Initial commit

---
+++ src/main/java/com/kirisaki/designproject/DesignprojectApplication.java
@@ -0,0 +1,13 @@
+package com.kirisaki.designproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kirisaki/designproject/DesignprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kirisaki.designproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
