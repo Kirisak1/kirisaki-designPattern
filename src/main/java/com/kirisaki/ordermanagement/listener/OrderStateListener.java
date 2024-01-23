93ae6e2da0f92ba3d1a7b135f8e96b7cd95c2de0
kirisaki <735647137@qq.com>
2024-01-23 16:28:25 +0800

    状态机监听类 完成

--- src/main/java/com/kirisaki/ordermanagement/listener/OrderStateListener.java
+++ src/main/java/com/kirisaki/ordermanagement/listener/OrderStateListener.java
@@ -1,10 +1,49 @@
 package com.kirisaki.ordermanagement.listener;
 
+
+import com.kirisaki.ordermanagement.state.OrderState;
+import com.kirisaki.ordermanagement.state.OrderStateChangeAction;
+import com.kirisaki.pojo.Order;
+import com.kirisaki.utils.RedisCommonProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.annotation.OnTransition;
 import org.springframework.statemachine.annotation.WithStateMachine;
 import org.springframework.stereotype.Component;
 
 @Component
-@WithStateMachine(name="orderStateMachine")
+@WithStateMachine(name = "orderStateMachine")
 public class OrderStateListener {
-
+    @Autowired
+    private RedisCommonProcessor redisCommonProcessor;
+    @OnTransition(source = "ORDER_WAIT_PAY", target = "ORDER_WAIT_SEND")
+    public boolean payToSend(Message<OrderStateChangeAction> message) {
+        Order order = (Order) message.getHeaders().get("order");
+        if (order.getOrderState() != OrderState.ORDER_WAIT_PAY) {
+            throw new UnsupportedOperationException("order state error !");
+        }
+        order.setOrderState(OrderState.ORDER_WAIT_SEND);
+        redisCommonProcessor.set(order.getOrderId(),order);
+        return true;
+    }
+    @OnTransition(source = "ORDER_WAIT_SEND", target = "ORDER_WAIT_RECEIVE")
+    public boolean sendToReceive(Message<OrderStateChangeAction> message) {
+        Order order = (Order) message.getHeaders().get("order");
+        if (order.getOrderState() != OrderState.ORDER_WAIT_SEND) {
+            throw new UnsupportedOperationException("order state error !");
+        }
+        order.setOrderState(OrderState.ORDER_WAIT_RECEIVE);
+        redisCommonProcessor.set(order.getOrderId(),order);
+        return true;
+    }
+    @OnTransition(source = "ORDER_WAIT_RECEIVE", target = "ORDER_FINISH")
+    public boolean receiveToFinish(Message<OrderStateChangeAction> message) {
+        Order order = (Order) message.getHeaders().get("order");
+        if (order.getOrderState() != OrderState.ORDER_WAIT_RECEIVE) {
+            throw new UnsupportedOperationException("order state error !");
+        }
+        order.setOrderState(OrderState.ORDER_FINISH);
+        redisCommonProcessor.remove(order.getOrderId());
+        return true;
+    }
 }
