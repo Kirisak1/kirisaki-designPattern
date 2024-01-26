ca2dfaaaf2d842f344989690d7c7aac3d2289a09
kirisaki <735647137@qq.com>
2024-01-26 23:51:03 +0800

    接入支付宝支付

--- src/main/java/com/kirisaki/controller/OrderController.java
+++ src/main/java/com/kirisaki/controller/OrderController.java
@@ -1,12 +1,21 @@
 package com.kirisaki.controller;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.kirisaki.pojo.Order;
 import com.kirisaki.service.OrderService;
+import com.kirisaki.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/order")
@@ -32,5 +41,34 @@ public class OrderController {
     @PostMapping("/receive")
     public Order receive(@RequestParam String orderId) {
         return orderService.receive(orderId);
+    }
+
+    @RequestMapping("/alipaycallback")
+    public String alipayCallback(HttpServletRequest request) throws AlipayApiException, UnsupportedEncodingException {
+        //获取返回回调参数
+        HashMap<String, String> params = new HashMap<>();
+        Map<String, String[]> requestParams = request.getParameterMap();
+        for (Iterator<String> iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = iter.next();
+            String valueStr = "";
+            String[] values = requestParams.get(name);
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i] : valueStr + values[i] + ",";
+            }
+            valueStr = new String(valueStr.getBytes("ISO-8859-1"), "UTF-8");
+            params.put(name, valueStr);
+        }
+        //验签
+        boolean signVerified = AlipaySignature.rsaCheckV1(params, Constants.ALIPAY_PUBLIC_KEY, "UTF-8", Constants.SIGN_TYPE);
+        if (signVerified) {
+            String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            String tradeNo = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            float total_amount = Float.parseFloat(new String(request.getParameter("total_amount").getBytes("ISO-8859-1"), "UTF-8"));
+
+            Order order = orderService.pay(out_trade_no);
+            return "支付成功页面跳转,当前订单为" + order;
+        }else {
+            throw new UnsupportedEncodingException("callback verify failed");
+        }
     }
 }
--- src/main/java/com/kirisaki/pay/strategy/AlipayStrategy.java
+++ src/main/java/com/kirisaki/pay/strategy/AlipayStrategy.java
@@ -1,13 +1,35 @@
 package com.kirisaki.pay.strategy;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.request.AlipayTradeWapPayRequest;
 import com.kirisaki.pojo.Order;
+import com.kirisaki.utils.Constants;
 
 /**
  * 支付宝支付
  */
-public class AlipayStrategy implements PayStrategyInterface{
+public class AlipayStrategy implements PayStrategyInterface {
+    // 建议直接抄官方文档
     @Override
     public String pay(Order order) {
-        return null;
+        AlipayClient alipayClient = new DefaultAlipayClient(Constants.ALIPAY_GATEWAY, Constants.APP_ID, Constants.APP_PRIVATE_KEY, "JSON", "UTF-8", Constants.ALIPAY_PUBLIC_KEY, Constants.SIGN_TYPE);
+        AlipayTradeWapPayRequest payRequest = new AlipayTradeWapPayRequest();
+
+        payRequest.setReturnUrl(Constants.CALLBACK_URL);
+
+        payRequest.setBizContent("{\"out_trade_no\": \"" + order.getOrderId() + "\"," +
+                "\"total_amount\":\"" + order.getPrice() + "\"," +
+                "\"subject\":\"" + "Kirisaki" + "\"," +
+                "\"body\":\"" + "商品描述" + "\","
+                + "\"product_code\":\"FAST_INSTANT_TRADE_PAY\"}");
+
+        try {
+            String result = alipayClient.pageExecute(payRequest, "GET").getBody();
+            return result;
+        } catch (AlipayApiException e) {
+            throw new UnsupportedOperationException("Alipay failed" + e);
+        }
     }
 }
