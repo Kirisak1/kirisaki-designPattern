a228aaaea08b72755a8a9edead4b042a262de60b
kirisaki <735647137@qq.com>
2024-01-28 17:27:32 +0800

    项目集成 apollo

--- src/main/java/com/kirisaki/DesignprojectApplication.java
+++ src/main/java/com/kirisaki/DesignprojectApplication.java
@@ -1,11 +1,13 @@
 package com.kirisaki;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableApolloConfig
 public class DesignprojectApplication {
 
     public static void main(String[] args) {
