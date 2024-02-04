fc06f5f5ad26b2dbbd074b06713b680ca5adbea2
kirisaki <735647137@qq.com>
2024-02-04 20:35:35 +0800

    完善需要填充的子类信息,但是没有接入业务, 且填充信息为示例代码.需要进行替换

--- src/main/java/com/kirisaki/ordermanagement/audit/PayOrderLog.java
+++ src/main/java/com/kirisaki/ordermanagement/audit/PayOrderLog.java
@@ -2,10 +2,16 @@ package com.kirisaki.ordermanagement.audit;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+
 @Component
 public class PayOrderLog extends AbstractAuditLogProcessor {
     @Override
     protected OrderAuditLog buildDetails(OrderAuditLog auditLog) {
+        HashMap<String, String> extraLog = new HashMap<>();
+        extraLog.put("payType", "支付宝");
+        extraLog.put("price", "100");
+        auditLog.setDetails(extraLog);
         return auditLog;
     }
 }
--- src/main/java/com/kirisaki/ordermanagement/audit/SendOrderLog.java
+++ src/main/java/com/kirisaki/ordermanagement/audit/SendOrderLog.java
@@ -2,10 +2,16 @@ package com.kirisaki.ordermanagement.audit;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+
 @Component
 public class SendOrderLog extends AbstractAuditLogProcessor {
     @Override
     protected OrderAuditLog buildDetails(OrderAuditLog auditLog) {
+        HashMap<String, String> extraLog = new HashMap<>();
+        extraLog.put("快递公司", "x 速递");
+        extraLog.put("快递编号", "100100100");
+        auditLog.setDetails(extraLog);
         return auditLog;
     }
 }
